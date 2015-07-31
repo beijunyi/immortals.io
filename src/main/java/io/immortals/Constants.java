21cee833316aa41a902f87cefaa984e904a45158
beijunyi <beijunyi@gmail.com>
2015-07-31 16:09:15 +0100

    pom settings

--- src/main/java/io/immortals/Constants.java
+++
@@ -1,4 +0,0 @@
-package io.immortals;
-
-public class Constants {
-}
---
+++ src/main/java/io/immortals/config/RuntimeConfig.java
@@ -0,0 +1,10 @@
+package io.immortals.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({BeanConfig.class})
+public class RuntimeConfig {
+
+}
